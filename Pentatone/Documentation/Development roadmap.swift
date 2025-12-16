117c2845b2aaa37f8918086a951efda291c9c25f
Chiel Zwinkels <chiel.zwinkels@gmail.com>
2025-12-16 18:59:08 +0100

    Changed app name

---
+++ Pentatone/Documentation/Development roadmap.swift
@@ -0,0 +1,35 @@
+//
+//  Development roadmap.swift
+//  Penta-Tone
+//
+//  Created by Chiel Zwinkels on 14/12/2025.
+//
+
+/*
+
+MAIN
+ - Fix iOS 15 compatibility
+ - Add oscillator waveform to parameters
+ - implement modulation generators
+ - implement modulators in paramater structure
+ - add initial touch and aftertouch sensitivity
+ - create preset management
+ - create developer view for sound editing/storing presets
+ - add macro control
+ - port engine to tonehive
+ - add in app documentation
+ 
+ 
+ 
+UI
+ - ET / JI: display as EQUAL / JUST
+ - Improve spacing/layout
+ - Implement scale type graphics display (raw shapes or image files?)
+ - Implement note name display
+ - Implement basic tooltip structure (toggle on/of in voice menu?)
+ 
+MINOR IMPROVEMENTS
+ - Change intonation display from ET/JI to EQUAL / JUST
+ - Check font warning
+ 
+ */
