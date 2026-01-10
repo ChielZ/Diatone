850ff9868c337bdb09dc643050e1f1bb1303d471
Chiel Zwinkels <chiel.zwinkels@gmail.com>
2026-01-10 23:02:29 +0100

    Setup for manual/documentation 3

--- Pentatone/Main/View code/V5 ManualView/V5 ManualView.swift
+++ Pentatone/Main/View code/V5 ManualView/V5 ManualView.swift
@@ -5,56 +5,7 @@
 //  Created by Chiel Zwinkels on 10/01/2026.
 //
 
-/*
- 
- INTRODUCTION
- - What is an Arithmophone?
- - What is Pentatone?
- 
- THE KEYBOARD
- - Touch sensitivity
- - Fold/Unfold
- 
- THE SCALES
- - Just intonation vs ET (short, with reference to background)
- - Keys
- - Scales (short, with a few examples and reference to background)
- - Rotation
- 
- THE SOUNDS
- - Preset selector
- - Macro sliders
- 
- THE SETTINGS
- - voice mode
- - octave
- - tune
- - The editor (short pro version explanation with reference to editor)
- 
- BACKGROUND
- - What is just intonation?
- - What is equal temperament?
- - The arithmophone pentatonic scale system
- - Scale diagrams
- 
- THE EDITOR
- - What is FM? (make skippable)
- - Preset management
- - Oscillators
- - Contour
- - Effects
- - Master
- - Mod Envelope
- - Aux Envelope
- - Voice LFO
- - Global LFO
- - Touch
- - Macro ranges
 
- 
- 
- 
- */
 
 import SwiftUI
 
