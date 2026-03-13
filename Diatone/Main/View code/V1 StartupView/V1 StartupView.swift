0e8915ebbc2b13273af6f58b1b7461edea4721bc
Chiel Zwinkels <chiel.zwinkels@gmail.com>
2026-03-13 02:51:27 +0100

    Improved startup screen

--- Diatone/Main/View code/V1 StartupView/V1 StartupView.swift
+++ Diatone/Main/View code/V1 StartupView/V1 StartupView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct StartupView: View {
     var body: some View {
-        ZStack{
-            Color("BackgroundColour").ignoresSafeArea()            
-            //Image("Penta-Tone icon dark")
-              //  .resizable().aspectRatio(contentMode: .fit)
-        }.statusBar(hidden: true)
+        Color("BackgroundColour")
+            .overlay {
+                Image("Diatone startup icon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 1024, maxHeight: 1024)
+            }
+            .ignoresSafeArea()
+            .statusBar(hidden: true)
     }
 }
 
